8d95234f47f7fc47e949eb8d3206be00e0fea581
GabeYuan <769318433@qq.com>
2024-09-13 17:55:37 +0800

    add(play): 按钮效果调试

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <vl-button />
-  <vl-card />
+  <div>
+    <h2>按钮</h2>
+    <hr />
+    <h3>类型</h3>
+    <vl-button>Default</vl-button>
+    <vl-button type="primary">primary</vl-button>
+    <vl-button type="success">success</vl-button>
+    <vl-button type="warn">warn</vl-button>
+    <vl-button type="danger">danger</vl-button>
+    <vl-button type="info">info</vl-button>
+  </div>
+  <!-- <vl-card /> -->
 </template>
